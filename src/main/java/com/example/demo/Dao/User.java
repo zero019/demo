a9a5542bc92d5afb63712c548d15af7938040e90
zero019 <2464633099@qq.com>
2023-01-31 07:44:40 +0800

    @Data @Builder

--- src/main/java/com/example/demo/Dao/User.java
+++ src/main/java/com/example/demo/Dao/User.java
@@ -2,21 +2,15 @@ package com.example.demo.Dao;
 
 import lombok.*;
 
-@Getter
-@Setter
-@ToString(exclude = {"email"},of = {"s","userName"})
-@EqualsAndHashCode(exclude = {"s"},of = {"userName"})
-//RequiredArgsConstructor 未赋值的final变量和NonNull变量生效
-@RequiredArgsConstructor
-@AllArgsConstructor
-//@NoArgsConstructor
 /**
  * 1. 可通过参数exclude排除一些属性
  * 2. 可通过参数of指定仅使用哪些属性
  * */
+@Data
+@Builder
+/**使用@data 可以不对final对象进行赋值，
+ * 除无参构造与全参构造外，之前所上传的知识点均拥有*/
 public class User {
-//    @Getter(AccessLevel.PROTECTED)
-//    @Setter
     static String s = "";
     //因为无参构造NoArgsConstructor，必须保证final有值，所以两者有冲突
     final String a ;
@@ -35,10 +29,11 @@ public class User {
     }
 
     public static void main (String[] args){
-        User user = new User("1",2);
-        user.setId(10);
-        user.setUserName("2312");
-        System.out.println(user.toString());
+//        User user = new User();
+        User user = User.builder().password("1").email("2").id(1).userName("222").build();
+//        user.setId(10);
+//        user.setUserName("2312");
+//        System.out.println(user.toString());
         user.test(null);
     }
 }
