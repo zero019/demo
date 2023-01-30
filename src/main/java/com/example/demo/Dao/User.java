1f050ce0ae64717a7a8b0c905d4144c26337c81f
zero019 <2464633099@qq.com>
2023-01-31 07:30:14 +0800

    参数构造，注意无参构造和final关键字未赋值会起冲突

--- src/main/java/com/example/demo/Dao/User.java
+++ src/main/java/com/example/demo/Dao/User.java
@@ -6,6 +6,10 @@ import lombok.*;
 @Setter
 @ToString(exclude = {"email"},of = {"s","userName"})
 @EqualsAndHashCode(exclude = {"s"},of = {"userName"})
+//RequiredArgsConstructor 未赋值的final变量和NonNull变量生效
+@RequiredArgsConstructor
+@AllArgsConstructor
+//@NoArgsConstructor
 /**
  * 1. 可通过参数exclude排除一些属性
  * 2. 可通过参数of指定仅使用哪些属性
@@ -14,7 +18,9 @@ public class User {
 //    @Getter(AccessLevel.PROTECTED)
 //    @Setter
     static String s = "";
-    final String a = "111";
+    //因为无参构造NoArgsConstructor，必须保证final有值，所以两者有冲突
+    final String a ;
+    @NonNull
     private Integer id;
     private String userName;
     private String password;
@@ -23,10 +29,16 @@ public class User {
     @Setter(AccessLevel.NONE)
     private String email;
 
+    //NonNull是对s进行的一次非空判断
+    public void test(@NonNull String s){
+        System.out.println(s);
+    }
+
     public static void main (String[] args){
-        User user = new User();
+        User user = new User("1",2);
         user.setId(10);
         user.setUserName("2312");
         System.out.println(user.toString());
+        user.test(null);
     }
 }
